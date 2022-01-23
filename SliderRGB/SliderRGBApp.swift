45d58a11fc2bdc681a9c977caf86d02b7fd8e6a0
mikhail19283 <mikhail19283@gmail.com>
2022-01-24 00:20:59 +0500

    Initial Commit

---
+++ SliderRGB/SliderRGBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderRGBApp.swift
+//  SliderRGB
+//
+//  Created by Mikhail Kamynin on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderRGBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
