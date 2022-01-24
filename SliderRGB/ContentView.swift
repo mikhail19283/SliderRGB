0f7735c6fefd3943a5faaf01805a0f627083e290
mikhail19283 <mikhail19283@gmail.com>
2022-01-24 23:39:41 +0500

    1

--- SliderRGB/ContentView.swift
+++ SliderRGB/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sliderRedColor = Double.random(in: 0...255)
+    @State private var sliderGreenColor = Double.random(in: 0...255)
+    @State private var sliderBlueColor = Double.random(in: 0...255)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack {
+                Rectangle()
+                    .padding()
+                    .foregroundColor(Color(red: sliderRedColor / 255, green: sliderGreenColor / 255, blue: sliderBlueColor / 255, opacity: 1.0))
+                
+                SliderView(value: $sliderRedColor, color: .red)
+                SliderView(value: $sliderGreenColor, color: .green)
+                SliderView(value: $sliderBlueColor, color: .blue)
+            }
+        }
     }
 }
 
@@ -19,3 +33,38 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct SliderView: View {
+    
+    @Binding var value: Double
+    @FocusState var isInputActive: Bool
+    var color: Color
+    
+    var body: some View {
+        
+        
+        HStack {
+            Text("\(value, specifier: "%.0f")")
+                .padding()
+            
+            Slider(value: $value, in: 0...255, step: 1)
+                .accentColor(color)
+            TextField("", value: $value, formatter: NumberFormatter())
+                .frame(width: 60.0)
+                .textFieldStyle(.roundedBorder)
+                .focused($isInputActive)
+                .keyboardType(.decimalPad)
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        
+                        Button("Done") {
+                            isInputActive = false
+                        }
+                    }
+                }
+        }
+    }
+}
+
