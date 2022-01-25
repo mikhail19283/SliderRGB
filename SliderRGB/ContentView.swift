badb2d968332900e40a288c2d44e1185c694b129
mikhail19283 <mikhail19283@gmail.com>
2022-01-25 07:19:11 +0500

    final version

--- SliderRGB/ContentView.swift
+++ SliderRGB/ContentView.swift
@@ -12,17 +12,31 @@ struct ContentView: View {
     @State private var sliderRedColor = Double.random(in: 0...255)
     @State private var sliderGreenColor = Double.random(in: 0...255)
     @State private var sliderBlueColor = Double.random(in: 0...255)
+    @FocusState private var isInputActive: Bool
     
     var body: some View {
         ZStack {
+            Color.teal.opacity(0.5)
+                .ignoresSafeArea()
             VStack {
                 Rectangle()
-                    .padding()
+                    .frame(width: 350, height: 200)
+                    .cornerRadius(30)
                     .foregroundColor(Color(red: sliderRedColor / 255, green: sliderGreenColor / 255, blue: sliderBlueColor / 255, opacity: 1.0))
                 
-                SliderView(value: $sliderRedColor, color: .red)
-                SliderView(value: $sliderGreenColor, color: .green)
-                SliderView(value: $sliderBlueColor, color: .blue)
+                SliderView(value: $sliderRedColor, isInputActive: $isInputActive, color: .red)
+                SliderView(value: $sliderGreenColor, isInputActive: $isInputActive, color: .green)
+                SliderView(value: $sliderBlueColor, isInputActive: $isInputActive, color: .blue)
+            }
+            .padding(EdgeInsets(top: -100, leading: 20, bottom: 0, trailing: 20))
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        isInputActive = false
+                    }
+                }
             }
         }
     }
@@ -38,32 +52,20 @@ struct ContentView_Previews: PreviewProvider {
 struct SliderView: View {
     
     @Binding var value: Double
-    @FocusState var isInputActive: Bool
+    var isInputActive: FocusState<Bool>.Binding
     var color: Color
     
     var body: some View {
-        
-        
         HStack {
             Text("\(value, specifier: "%.0f")")
-                .padding()
-            
+                .frame(width: 50)
             Slider(value: $value, in: 0...255, step: 1)
                 .accentColor(color)
             TextField("", value: $value, formatter: NumberFormatter())
-                .frame(width: 60.0)
+                .frame(width: 50.0)
                 .textFieldStyle(.roundedBorder)
-                .focused($isInputActive)
+                .focused(isInputActive)
                 .keyboardType(.decimalPad)
-                .toolbar {
-                    ToolbarItemGroup(placement: .keyboard) {
-                        Spacer()
-                        
-                        Button("Done") {
-                            isInputActive = false
-                        }
-                    }
-                }
         }
     }
 }
